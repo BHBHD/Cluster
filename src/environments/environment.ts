9530e70ed025ff3d27f79abac065fae3ef46c0eb
BHBHD <BHBHD65@gmail.com>
2022-08-28 13:38:01 +0530

    Cors Fix and other browser fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:8000',
+  endpoint: 'http://18.206.118.16',
   firebase: {
     projectId: 'cluster-version-0',
     appId: '1:20340317994:web:8ce187af8121cd09c08853',
